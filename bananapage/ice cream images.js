83abf4c0a5c66537c8da0381740d0a47edd5b1d2
lisasantaniello <123006916+lisasantaniello@users.noreply.github.com>
2023-04-19 16:47:15 -0700

    uploading bananasplit page

---
+++ bananapage/ice cream images.js
@@ -0,0 +1,10 @@
+const scoop = document.getElementById('scoop'); // Get the image element
+
+scoop.addEventListener('click', () => { // Add click event listener to image
+  scoop.src = 'vanillascoop.png'; // Change the image source when clicked
+});
+const scoop = document.getElementById('scoop'); // Get the image element
+
+scoop.addEventListener('click', () => { // Add click event listener to image
+  scoop.src = 'vanillascoop.png'; // Change the image source when clicked
+});
