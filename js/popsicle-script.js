e5e968e4350fef54ce03764b17c6f36aca3ebc26
sherryxuchou <123006713+sherryxuchou@users.noreply.github.com>
2023-04-20 22:45:51 -0700

    character animations + finished popsicle page

---
+++ js/popsicle-script.js
@@ -0,0 +1,14 @@
+	$( function() {
+			$( "#draggable-1" ).draggable();
+			$( "#draggable-2" ).draggable();
+			$( "#draggable-3" ).draggable();
+			$( "#draggable-4" ).draggable();
+			$( "#draggable-5" ).draggable();
+			$( "#draggable-6" ).draggable();
+			$( "#draggable-7" ).draggable();
+			$( "#draggable-8" ).draggable();
+			$( "#draggable-9" ).draggable();
+			$( "#draggable-10" ).draggable(); 
+			$( "#draggable-11" ).draggable(); 
+			$( "#draggable-12" ).draggable(); 
+		} );
